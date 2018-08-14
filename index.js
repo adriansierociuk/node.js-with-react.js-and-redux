c2d7b1e1da39b0a7b566485a9f669f59c6bacf8e
Adrian Sierociuk <adrian@macbook-pro-adrian.home>
2018-08-14 17:04:43 +0200

    solve error

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ app.get('/', (req, res) =>{
 */
 
 if(process.env.NODE_ENV === 'production'){
+    const path = require('path'); //We need path earlier for this!
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    //No more changes from here on now
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client'
+, 'build', 'index.html'))
+    });
+}
+
+/*
+if(process.env.NODE_ENV === 'production'){
 	// Express will serve up production assets
 	// like our main.js file, or main.css file
 	app.use(express.static('client/build'));
@@ -46,7 +57,7 @@ if(process.env.NODE_ENV === 'production'){
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
-}
+} */
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
